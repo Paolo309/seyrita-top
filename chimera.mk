a0f7bef1b673e817a3fdaab7b83e894de404684d
Paolo Galfano <pgalfano@student.ethz.ch>
2025-12-05 18:22:58 +0100

    fix missing init for xilinx target

--- chimera.mk
+++ chimera.mk
@@ -154,10 +154,14 @@ gen-mxita-data:
 #################################
 # Phonies for the entire system #
 #################################
-CHIM_HW_ALL = chs-hw-init snitch-hw-init chim-bootrom-init chs-sim-all
+CHIM_HW_INIT = chs-hw-init snitch-hw-init chim-bootrom-init
+CHIM_HW_ALL = $(CHIM_HW_INIT) chs-sim-all
 CHIM_SW_ALL = chim-sw
 CHIM_ALL += $(CHIM_HW_ALL) $(CHIM_SW_ALL) chim-sim
 CHIM_CLEAN += chim-sw-clean chim-sim-clean
+
+.PHONY: chim-hw-init
+chim-hw-init: $(CHIM_HW_INIT) ## Initialize chimera hardware (no sim)
 
 .PHONY: chim-all
 chim-all: $(CHIM_ALL) ## Generate full chimera infrastructure
