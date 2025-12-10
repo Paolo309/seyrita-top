356af2efca2fe35aacceb3c2e1c266fa2a6bd2f1
Paolo Galfano <pgalfano@student.ethz.ch>
2025-12-10 12:46:28 +0100

    refactor CI and add loop on booking

--- utils/fpga/test-fpga.sh
+++ utils/fpga/test-fpga.sh
@@ -2,15 +2,32 @@
 
 WORKDIR="~/gitlab-ci"
 BOARD_CLASS=vcu128
+# TODO set as CI variables?
+REMOTE_HOST=boardberg.ee.ethz.ch
+REMOTE_USER=msc25h20
+
+run_remote_command() {
+    ssh $REMOTE_USER@$REMOTE_HOST "$1"
+}
 
 ################
 # BOOK AND RUN #
 ################
 
-# TODO put in a "infinite" loop to retry booking if no board is available, every X minutes (CI timeout will stop the loop and fail)
-echo "Booking and running FPGA board of class $BOARD_CLASS ..."
-result=$(ssh msc25h20@boardberg.ee.ethz.ch "bash -lc 'bash -s $WORKDIR $BOARD_CLASS'" < utils/fpga/book-and-run.sh)
-status=$?
+# book-and-run.sh returns 0 if successful, 254 if no board available, 253 if booking fails
+status=254
+while [ $status -eq 254 ]; do
+    echo "Booking and running FPGA board of class $BOARD_CLASS ..."
+    result=$(run_remote_command "bash -lc 'bash -s $WORKDIR $BOARD_CLASS'" < utils/fpga/book-and-run.sh)
+    status=$?
+
+    # no free board available
+    if [ $status -eq 254 ]; then
+        echo "$result"
+        echo "Retrying in 5 minutes ..."
+        sleep 5
+    fi
+done
 
 if [ $status -ne 0 ]; then
     echo "$result"
@@ -31,10 +48,10 @@ echo "  - GDB: $GDB_PORT"
 ###################
 
 echo "Programming bistream ..."
-make chim-xilinx-program-$BOARD_CLASS CHIM_XILINX_HWS_URL=boardberg.ee.ethz.ch:$TCP_PORT
+make chim-xilinx-program-$BOARD_CLASS CHIM_XILINX_HWS_URL=$REMOTE_HOST:$TCP_PORT
 
 echo "Sending reset ..."
-make chim-xilinx-reset-$BOARD_CLASS CHIM_XILINX_HWS_URL=boardberg.ee.ethz.ch:$TCP_PORT
+make chim-xilinx-reset-$BOARD_CLASS CHIM_XILINX_HWS_URL=$REMOTE_HOST:$TCP_PORT
 
 ##################################
 # PREPARE GDB AND LAUNCH OPENOCD #
@@ -42,18 +59,18 @@ make chim-xilinx-reset-$BOARD_CLASS CHIM_XILINX_HWS_URL=boardberg.ee.ethz.ch:$TC
 
 # XXX use a specific .gdbinit for CI
 cp "$CHISDK_ROOT/.gdbinit" .
-# sed -i "s/target extended-remote localhost:3333/target extended-remote boardberg.ee.ethz.ch:$GDB_PORT/" .gdbinit
+# sed -i "s/target extended-remote localhost:3333/target extended-remote $REMOTE_HOST:$GDB_PORT/" .gdbinit
 sed -i "s/target extended-remote localhost:3333/target extended-remote localhost:$GDB_PORT/" .gdbinit
 printf "\noc\nload\ncontinue\nquit\n" >> .gdbinit
 
 echo "[DEBUG]"
 cat ".gdbinit"
 
-scp .gdbinit msc25h20@boardberg.ee.ethz.ch:$WORKDIR/
-scp utils/fpga/openocd.*.tcl msc25h20@boardberg.ee.ethz.ch:$WORKDIR/
-scp $CHISDK_ROOT/build-ASIC/bin/test_mxita_app msc25h20@boardberg.ee.ethz.ch:$WORKDIR/
+scp .gdbinit $REMOTE_USER@$REMOTE_HOST:$WORKDIR/
+scp utils/fpga/openocd.*.tcl $REMOTE_USER@$REMOTE_HOST:$WORKDIR/
+scp $CHISDK_ROOT/build-ASIC/bin/test_mxita_app $REMOTE_USER@$REMOTE_HOST:$WORKDIR/
 
-OPENOCD_PID=$(ssh msc25h20@boardberg.ee.ethz.ch "sh -c 'cd $WORKDIR && nohup openocd -f msc25h20-$BOARD-openocd.tcl -f openocd.$BOARD_CLASS.tcl >/dev/null 2>&1 & echo \$!'")
+OPENOCD_PID=$(run_remote_command "sh -c 'cd $WORKDIR && nohup openocd -f $REMOTE_USER-$BOARD-openocd.tcl -f openocd.$BOARD_CLASS.tcl >/dev/null 2>&1 & echo \$!'")
 echo "Started OpenOCD with PID $OPENOCD_PID"
 
 # to be sure OpenOCD is ready
@@ -64,19 +81,22 @@ sleep 2
 ################
 
 echo "Starting FPGA tests ..."
-ssh msc25h20@boardberg.ee.ethz.ch "cd $WORKDIR && gdb-multiarch -iex 'set auto-load safe-path /' test_mxita_app" | tee gdb_output.log
+run_remote_command "cd $WORKDIR && gdb-multiarch -iex 'set auto-load safe-path /' test_mxita_app" | tee gdb_output.log
+
+# TODO add minicon process, writing output on a transcript to show at the end
+# TODO add timeout to avoid hanging forever if it ever happens
 
 ############
 # CLEAN UP #
 ############
 
 echo "Killing OpenOCD with PID $OPENOCD_PID"
-ssh msc25h20@boardberg.ee.ethz.ch "kill $OPENOCD_PID"
+run_remote_command "kill $OPENOCD_PID"
 
 rm -rf $WORKDIR/*
 
 echo "Releasing board $BOARD ..."
-ssh msc25h20@boardberg.ee.ethz.ch "bash -lc 'fpga release -b $BOARD'"
+run_remote_command "bash -lc 'fpga release -b $BOARD'"
 
 RETURN_VALUE=$(cat gdb_output.log | grep "\[GDB\] Return Value:" | tail -n 1 | awk '{print $5}')
 
